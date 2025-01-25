d2a66d4c14dd23655671f4aba2258a36dfca66b6
Robert Lane <rob@lanosmedia.co.uk>
2025-01-25 15:55:40 +0000

    timeout changed

--- src/DeepSeekServiceProvider.php
+++ src/DeepSeekServiceProvider.php
@@ -5,6 +5,7 @@ namespace Lanos\DeepSeekClient;
 use Illuminate\Support\ServiceProvider;
 use Lanos\DeepSeekClient\Interfaces\DeepSeekClientInterface;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Config;
 
 class DeepSeekServiceProvider extends ServiceProvider
 {
@@ -12,8 +13,10 @@ class DeepSeekServiceProvider extends ServiceProvider
     {
         $this->app->singleton(DeepSeekClientInterface::class, function ($app) {
             return new DeepSeekClient(
-                new Client(),
-                config('deepseek.api_key')
+                new Client([
+                    'timeout' => 60,
+                ]),
+                Config::get('deepseek.api_key')
             );
         });
     }
